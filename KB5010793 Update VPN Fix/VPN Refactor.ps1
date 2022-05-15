cd2adfe1c132dad8bf01ef254d2a07ff8bf5719e
ron <davis.ronal@ronal.com>
2022-05-15 17:31:05 -0400

    Refactor Complete

--- KB5010793 Update VPN Fix/VPN Refactor.ps1
+++ KB5010793 Update VPN Fix/VPN Refactor.ps1
@@ -3,7 +3,7 @@ $Win10_19043_21H1x64_Address = "http://download.windowsupdate.com/d/msdownload/u
 $Win10_19044_21H2x64_Address = "http://download.windowsupdate.com/d/msdownload/update/software/updt/2022/01/windows10.0-kb5010793-x64_3bae2e811e2712bd1678a1b8d448b71a8e8c6292.msu"
 $Win11_22000x64_Address = "http://download.windowsupdate.com/d/msdownload/update/software/updt/2022/01/windows10.0-kb5010795-x64_7fd6ce84756ac03585cc012568979eb08cc6d583.msu"
 
-$UpdateFile = "C:\VPNFix"
+$UpdateFile = "C:\VPNFix\"
 
 $Win10_19042_20H2x64_location = $UpdateFile + "windows10.0-kb5010793-x64_3bae2e811e2712bd1678a1b8d448b71a8e8c6292.msu"
 $Win10_19043_21H1x64_location = $UpdateFile + "windows10.0-kb5010793-x64_3bae2e811e2712bd1678a1b8d448b71a8e8c6292.msu"
@@ -34,14 +34,14 @@ function Download-File {
 function Download-VPNFix {
 
     $winbuild = Get-WindowsBuildNumber
-
+    
     if ($winbuild.buildnumber -eq 19042){
-
+        
         Download-File -URL $Win10_19042_20H2x64_Address  -Location $Win10_19042_20H2x64_location
         
     }
     elseif ($winbuild.buildnumber -eq 19043){
-
+        
         Download-File -URL $Win10_19043_21H1x64_Address -Location $Win10_19043_21H1x64_location
 
     }
@@ -54,6 +54,12 @@ function Download-VPNFix {
         
         Download-File -URL $Win11_22000x64_Address -Location $Win11_22000x64_location
         
+    }
+    else {
+        
+        Write-host "This build does not have a VPN fix."
+        Return
+                
     }
 
     Start-Sleep 5
@@ -76,4 +82,4 @@ Download-VPNFix
 
 $VPNFile = Get-ChildItem $UpdateFile
 
-Install-VPNFix -VPNFixFileLocation $VPNFile
+Install-VPNFix -VPNFixFileLocation $VPNFile.FullName
