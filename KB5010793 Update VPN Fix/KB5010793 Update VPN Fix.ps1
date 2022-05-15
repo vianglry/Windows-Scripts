5a7be11442ea2345954ea1ce954dfa91cfb0ca02
ron <davis.ronal@ronal.com>
2022-05-15 17:37:47 -0400

    renamed

--- KB5010793 Update VPN Fix/KB5010793 Update VPN Fix.ps1
+++ KB5010793 Update VPN Fix/KB5010793 Update VPN Fix.ps1
@@ -1,70 +1,85 @@
-
-$Win10User = ((Get-WMIObject -ClassName Win32_ComputerSystem).Username).Split('\')[1]
-
-#win10 19042
 $Win10_19042_20H2x64_Address = "http://download.windowsupdate.com/d/msdownload/update/software/updt/2022/01/windows10.0-kb5010793-x64_3bae2e811e2712bd1678a1b8d448b71a8e8c6292.msu"
-
-$Win10_19042_20H2x64_location = "C:\users\$win10user\downloads\windows10.0-kb5010793-x64_3bae2e811e2712bd1678a1b8d448b71a8e8c6292.msu"
-
-#win10 19043
 $Win10_19043_21H1x64_Address = "http://download.windowsupdate.com/d/msdownload/update/software/updt/2022/01/windows10.0-kb5010793-x64_3bae2e811e2712bd1678a1b8d448b71a8e8c6292.msu"
-
-$Win10_19043_21H1x64_location = "C:\users\$win10user\downloads\windows10.0-kb5010793-x64_3bae2e811e2712bd1678a1b8d448b71a8e8c6292.msu"
-
-#Win10 19044
 $Win10_19044_21H2x64_Address = "http://download.windowsupdate.com/d/msdownload/update/software/updt/2022/01/windows10.0-kb5010793-x64_3bae2e811e2712bd1678a1b8d448b71a8e8c6292.msu"
-
-$Win10_19044_21H2x64_location = "C:\users\$win10user\downloads\windows10.0-kb5010793-x64_3bae2e811e2712bd1678a1b8d448b71a8e8c6292.msu"
-
-
-#win11
 $Win11_22000x64_Address = "http://download.windowsupdate.com/d/msdownload/update/software/updt/2022/01/windows10.0-kb5010795-x64_7fd6ce84756ac03585cc012568979eb08cc6d583.msu"
 
-$Win11_22000x64_location = "C:\users\$win10user\downloads\windows10.0-kb5010795-x64_7fd6ce84756ac03585cc012568979eb08cc6d583.msu"
+$UpdateFile = "C:\VPNFix\"
 
-$winbuild = Get-CimInstance Win32_OperatingSystem | select -Property buildnumber
+$Win10_19042_20H2x64_location = $UpdateFile + "windows10.0-kb5010793-x64_3bae2e811e2712bd1678a1b8d448b71a8e8c6292.msu"
+$Win10_19043_21H1x64_location = $UpdateFile + "windows10.0-kb5010793-x64_3bae2e811e2712bd1678a1b8d448b71a8e8c6292.msu"
+$Win10_19044_21H2x64_location = $UpdateFile + "windows10.0-kb5010793-x64_3bae2e811e2712bd1678a1b8d448b71a8e8c6292.msu"
+$Win11_22000x64_location = $UpdateFile + "windows10.0-kb5010795-x64_7fd6ce84756ac03585cc012568979eb08cc6d583.msu"
 
-if ($winbuild.buildnumber -eq 19042){
 
-
-#win10 19042
-Write-host "Windows 10 Build 19042 found. Starting Download."
-
-Sleep 5
-
-Invoke-WebRequest -Uri $Win10_19042_20H2x64_Address -OutFile $Win10_19042_20H2x64_location -v
-
-Write-host "Download Complete"
-
-Sleep 5
- 
-Write-host "Starting the Install"
-
-wusa.exe $Win10_19042_20H2x64_location /q /warnrestart
-
+function Get-WindowsBuildNumber {
+  
+    Get-CimInstance Win32_OperatingSystem | Select-Object -Property buildnumber
+    
 }
-elseif ($winbuild.buildnumber -eq 19043){
 
+function Download-File {
+    Param (
+        [string]$URL,
+        [String]$Location
+    )
 
-#win10 19043
-Write-host "Windows 10 Build 19043 found. Starting Download."
-
-Sleep 5
-
-Invoke-WebRequest -Uri $Win10_19043_21H1x64_Address -OutFile $Win10_19043_21H1x64_location -v
-
-Write-host "Download Complete"
-
-Sleep 5
- 
-Write-host "Starting the Install"
-
-wusa.exe $Win10_19043_21H1x64_location /q /warnrestart
+    $Winbuild = Get-WindowsBuildNumber
+    Write-host "Windows Build "$winbuild.buildnumber" found. Starting Download."
+    Start-Sleep 5
+    Invoke-WebRequest -Uri $URL -OutFile $Location -v
+    Write-host "Download Complete"
+    Start-Sleep 5
 }
-elseif ($winbuild.buildnumber -eq 19044){
 
+function Download-VPNFix {
 
-#win10 19044
-Write-host "Windows 10 Build 19044 found. Starting Download."
+    $winbuild = Get-WindowsBuildNumber
+    
+    if ($winbuild.buildnumber -eq 19042){
+        
+        Download-File -URL $Win10_19042_20H2x64_Address  -Location $Win10_19042_20H2x64_location
+        
+    }
+    elseif ($winbuild.buildnumber -eq 19043){
+        
+        Download-File -URL $Win10_19043_21H1x64_Address -Location $Win10_19043_21H1x64_location
 
+    }
+    elseif ($winbuild.buildnumber -eq 19044){
+        
+        Download-File -URL $Win10_19044_21H2x64_Address -Location $Win10_19044_21H2x64_location
+        
+    }
+    elseif ($winbuild.buildnumber -eq 22000){
+        
+        Download-File -URL $Win11_22000x64_Address -Location $Win11_22000x64_location
+        
+    }
+    else {
+        
+        Write-host "This build does not have a VPN fix."
+        Return
+                
+    }
+
+    Start-Sleep 5
 }
+
+function Install-VPNFix {
+    param (
+        [string]$VPNFixFileLocation
+    )
+    
+    Write-host "Starting the Install"
+
+    wusa.exe $VPNFixFileLocation /q /warnrestart
+    
+}
+
+
+
+Download-VPNFix
+
+$VPNFile = Get-ChildItem $UpdateFile
+
+Install-VPNFix -VPNFixFileLocation $VPNFile.FullName
