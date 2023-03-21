6d4d79541f830c19b1d39de09c4eaa430666dd94
ron <davis.ronal@ronal.com>
2023-03-20 20:33:57 -0400

    adjusted remove-user workflow to add the explicit path to the backup registry file

--- Remove-User-Account/Remove-FullUserAccountDetails.tests.ps1
+++ Remove-User-Account/Remove-FullUserAccountDetails.tests.ps1
@@ -19,7 +19,7 @@ Describe "Test for the existence of a registry backup" {
     Context "When the user exists" {
         It "should return true" {
             New-ProfileListRegKeyBackup
-            Test-path $ProfileListBackupFolder | Should -Be $true
+            Test-path "C:\ProfileList.reg" | Should -Be $true
         }
     }
 }
